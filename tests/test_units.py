f632d8e90c02e8df37582dc65ba4f98a4cca7710
Austin M. Matherne <AustinMatherne@Gmail.com>
2013-06-04 22:00:07 -0500

    Add test for xbrl.add_namespace function.

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 import unittest
+from thinX import namespace
 from thinX import xbrl
 
 
 class Units(unittest.TestCase):
 
     def setUp(self):
+        self.instance_file = "assets/abc-20130331.xml"
         self.unit_config_file = "assets/units.ini"
         self.units_dictionary = xbrl.get_units(self.unit_config_file)
+        self.tree = namespace.parse_xmlns(self.instance_file)
+        self.root = self.tree.getroot()
 
     def test_get_units(self):
         self.assertIn("TestMeasures", self.units_dictionary)
@@ -22,7 +26,27 @@ class Units(unittest.TestCase):
         self.assertEqual(testMeasures["Measures"], ["M1", "M2", "M3"])
 
     def test_add_namespace(self):
-        pass
+        prefix = "xmlns:test"
+        ns = "http://www.example.org/1989/instance"
+        measures = ["Y", "t", "T", "fake", "acre"]
+        log = xbrl.add_namespace(self.root, prefix, ns, measures)
+
+        self.assertEqual(self.root.get(prefix), ns)
+
+        unit_elem = "{http://www.xbrl.org/2003/instance}unit[@id='%(unit)s']/*"
+
+        year = self.root.findtext(unit_elem % {"unit": "Year"})
+        tonne = self.root.findtext(unit_elem % {"unit": "Tonne"})
+        ton = self.root.findtext(unit_elem % {"unit": "Ton"})
+        acre = self.root.findtext(unit_elem % {"unit": "Acre"})
+        acre = self.root.findtext(unit_elem % {"unit": "Acre"})
+        acre = self.root.findtext(unit_elem % {"unit": "Acre"})
+
+        self.assertEqual(year, "test:Y")
+        self.assertEqual(tonne, "test:t")
+        self.assertEqual(ton, "test:T")
+        self.assertEqual(acre, "test:acre")
+
 
     def test_extended_measures(self):
         pass
