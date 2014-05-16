831b6eaa199b348d0c0463a9a61fde6b7e7fb7e4
Austin M. Matherne <AustinMatherne@Gmail.com>
2014-05-16 16:50:01 -0500

    Test: move calc element retrieval to setUp()

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -9,8 +9,11 @@ class Calculations(unittest.TestCase):
 
     def setUp(self):
         self.instance_file = "assets/abc-20130331.xml"
+        cal_linkbase = xbrl.get_linkbase(self.instance_file, "cal")
         tree = namespace.parse_xmlns(self.instance_file)
+        cal_tree = namespace.parse_xmlns(cal_linkbase)
         self.root = tree.getroot()
+        self.cal_root = cal_tree.getroot()
 
     def test_get_linkbase(self):
         schema = xbrl.get_linkbase(self.instance_file, "xsd")
@@ -26,9 +29,6 @@ class Calculations(unittest.TestCase):
         self.assertEqual("assets/abc-20130331_lab.xml", lab_linkbase)
 
     def test_get_calcs(self):
-        cal_linkbase = xbrl.get_linkbase(self.instance_file, "cal")
-        cal_tree = namespace.parse_xmlns(cal_linkbase)
-        cal_root = cal_tree.getroot()
         linkrole = "http://www.example.com/role/BalanceSheetComponents" \
                         "InventoriesDetails"
         total = "us-gaap_InventoryNet"
@@ -37,31 +37,24 @@ class Calculations(unittest.TestCase):
             ("us-gaap_InventoryWorkInProcessAndRawMaterialsNetOfReserves", "1")
         ]
 
-        calc_linkroles = xbrl.get_calcs(cal_root)
+        calc_linkroles = xbrl.get_calcs(self.cal_root)
         total_elements = []
         for calc_linkrole in calc_linkroles:
             for calc_total in calc_linkroles[calc_linkrole]:
                 total_elements.append(calc_total)
 
-        #Check for the correct amount of linkroles.
         self.assertEqual(10, len(calc_linkroles))
-        #Check that the total element is in the linkrole.
         self.assertIn(total, calc_linkroles[linkrole])
-        #Check for the correct amount of total elements.
         self.assertEqual(31, len(total_elements))
-        #Check that the elements that add to the total element are in the list.
         self.assertEqual(elems_to_add, calc_linkroles[linkrole][total])
 
     def test_dup_calcs(self):
-        cal_linkbase = xbrl.get_linkbase(self.instance_file, "cal")
-        cal_tree = namespace.parse_xmlns(cal_linkbase)
-        cal_root = cal_tree.getroot()
         expected_dup_calcs = {
             "us-gaap_OtherComprehensiveIncomeLossAvailableForSaleSecurities" \
             "AdjustmentNetOfTax": 0,
             "us-gaap_NetCashProvidedByUsedInInvestingActivities": 1,
             "us-gaap_NetIncomeLoss": 0
         }
-        duplicate_calcs = xbrl.dup_calcs(cal_root)
+        duplicate_calcs = xbrl.dup_calcs(self.cal_root)
 
         self.assertEqual(expected_dup_calcs, duplicate_calcs)
